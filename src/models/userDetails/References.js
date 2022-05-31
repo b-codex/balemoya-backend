8961db08ccb3b6e2850078604a70e960df9f07ef
bisrat fekede <it.bisrat.fekede@gmail.com>
2022-05-31 18:35:40 +0300

    reference model modified to fullname and phone number

--- src/models/userDetails/References.js
+++ src/models/userDetails/References.js
@@ -5,14 +5,6 @@ const ReferencesSchema = new mongoose.Schema(
       type: String,
       required: [true, "FullName is required"],
     },
-    position: {
-      type: String,
-      required: [true, "Position is required"],
-    },
-    email: {
-      type: String,
-      required: [true, "Email is required"],
-    },
     phoneNumber: {
       type: String,
       required: [true, "PhoneNumber is required"],
